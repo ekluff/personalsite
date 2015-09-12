f8e41dd02ebc1fc0078d8a6b22d017f2e5492b2e
Evan Clough <clough.evan@gmail.com>
2015-09-11 23:14:55 -0700

    Comment out code that deletes original image after upload.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,15 @@
 class Image < ActiveRecord::Base
   has_attached_file :asset, styles: { large: "2000x2000>", medium: "768x768>", small: "240x240>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates :asset, attachment_presence: true
   validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
 
-  after_save :destroy_original
+  # Uncomment below and corresponding private method to delete original file after upload. Will not delete parent directory to original image.
+  # after_save :destroy_original
 
   private
 
-  def destroy_original
-    File.unlink(self.asset.path)
-  end
+  # def destroy_original
+  #   File.unlink(self.asset.path)
+  # end
 
 end
