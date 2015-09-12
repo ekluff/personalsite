e6124097711348c4c24e57fc624fcf5a1690f5e7
Evan Clough <clough.evan@gmail.com>
2015-09-11 23:38:26 -0700

    Run migration to create exifs table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911225226) do
+ActiveRecord::Schema.define(version: 20150912063340) do
+
+  create_table "exifs", force: :cascade do |t|
+    t.string   "camera"
+    t.string   "software"
+    t.integer  "iso"
+    t.string   "shutter"
+    t.string   "aperture"
+    t.datetime "taken"
+    t.string   "exposure_bias"
+    t.boolean  "flash"
+    t.string   "focal_length"
+    t.string   "lens"
+    t.integer  "image_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "exifs", ["image_id"], name: "index_exifs_on_image_id"
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at",         null: false
