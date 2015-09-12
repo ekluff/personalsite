4b39ba302b11a858b0848e78c06ae6346583e194
Evan Clough <clough.evan@gmail.com>
2015-09-11 22:31:09 -0700

    Change image asset styles.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :asset, styles: { large: "1920x1080", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :asset, styles: { large: "2000x2000>", medium: "768x768>", small: "240x240>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
 
   after_save :destroy_original
