d0b745bc029c5c6db50975728ba864e83472a119
Evan Clough <clough.evan@gmail.com>
2015-09-11 22:25:29 -0700

    Add method to destory original image file after saving resized versions.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ActiveRecord::Base
   has_attached_file :asset, styles: { large: "1920x1080", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
+
+  after_save :destroy_original
+
+  private
+
+  def destroy_original
+    File.unlink(self.asset.path)
+  end
+
 end
