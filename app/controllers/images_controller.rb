d07954655825e50a0038973a464d00728a203c7d
Evan Clough <clough.evan@gmail.com>
2015-09-11 17:20:26 -0700

    Configure paperclip to work with imagemagick

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -54,6 +54,8 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
+    @image.asset = nil
+    @image.save
     @image.destroy
     respond_to do |format|
       format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
-  has_attached_file :asset, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :asset, styles: { large: "1920x1080", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
 end
